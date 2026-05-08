fd51b9d49de5de0e0910bfe8558bc2c4753aa988
Lim-9S-AY2526 <lim.es@obmontessori.edu.ph>
2026-05-08 20:50:15 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let attempt_counter = 0;
+
 function authenticate() {
     let username, pwd, granted, denied;
     username = document.getElementById("uname").value;
@@ -5,6 +7,7 @@ function authenticate() {
     granted = "Welcome " + username + "!";
     denied = "Access denied. Account locked after 3 attempts!";
 
+  
     let status = "";
 
     if (username === "student01" && pwd === "12345678") {
@@ -13,7 +16,7 @@ function authenticate() {
         status = "failed";
     }
 
-
+   
     switch (status) {
         case "success":
             alert(granted);
@@ -27,10 +30,11 @@ function authenticate() {
             document.getElementById("display").innerHTML =
                 "Incorrect username and/or password. Attempt " + attempt_counter + " of 3";
 
+          
             for (let i = attempt_counter; i >= 3; i++) {
                 alert(denied);
                 document.getElementById("loginBTN").disabled = true;
-                break; 
+                break;
             }
             break;
     }
